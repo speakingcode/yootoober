92e64b071a9aa88871ea9c0d2f957fb00dc9c2c6
speakingcode <dlissner@gmail.com>
2016-05-25 14:49:10 -0500

    Add initial search service

---
+++ src/app/components/search/search.service.js
@@ -0,0 +1,28 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('yootoober')
+    .service('SearchService', SearchService);
+
+  /** @ngInject */
+  function SearchService($http, LoginService) {
+    var _videos = [];
+
+    this.search = function(query) {
+      $http.get(
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video" +
+        "&q="             + query +
+        "&access_token="  + LoginService.accessToken()
+      )
+      .success(function(response) { console.log(response);});
+    };
+
+    this.videos = function(videos) {
+      if(arguments.length === 0)
+        return _videos;
+
+      _videos = videos;
+    };
+  }
+})();
