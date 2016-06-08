0596c640882ec56f662601f52988a1226f40d8b7
speakingcode <dlissner@gmail.com>
2016-06-08 03:02:21 -0500

    Implement sorting and location filter for search

--- src/app/components/search/search.service.js
+++ src/app/components/search/search.service.js
@@ -9,26 +9,31 @@
   function SearchService($http, $q, LoginService) {
     var _searches = {};
 
-    this.search = function(query) {
-
+    this.search = function(query, locationFilter = "", sortOption = "relevance") {
       if (!query) {
         return $q.when([]);
       }
 
-      if (_searches[query]) {
-        return $q.when(_searches[query]);
+      if (_searches[query] && _searches[query][sortOption + locationFilter]) {
+        return $q.when(_searches[query][sortOption + locationFilter]);
       }
 
       return $http.get(
-        "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video" +
+        "https://www.googleapis.com/youtube/v3/search" + 
+        "?part="          + "snippet" +
+        "&type="          + "video" +
         "&q="             + query +
+        (locationFilter ? "&location="  + locationFilter + "&locationRadius=20mi" : "") +
+        (sortOption     ? "&order="     + sortOption : "") +
+        "&maxResults="    + "50" +
         "&access_token="  + LoginService.accessToken()
       )
       .then(function(response) {
         console.log(response);
-        _searches[query] = response.data.items;
+        _searches[query] = _searches[query] || {};
+        _searches[query][sortOption + locationFilter] = response.data.items;
         console.log(_searches);
-        return _searches[query];
+        return _searches[query][sortOption + locationFilter];
       });
     };
   }
--- src/app/search/search.controller.js
+++ src/app/search/search.controller.js
@@ -9,19 +9,33 @@
   function SearchController($scope, $state, $stateParams, SearchService) {
     var searchCtrl = this;
 
-    searchCtrl.query  = $stateParams.searchQuery || "";
+    searchCtrl.query          = $stateParams.searchQuery || "";
+
+    searchCtrl.sortOptions    = ["relevance", "date", "rating"];
+    searchCtrl.sortOption     = $stateParams.sortOption || "relevance";
+    searchCtrl.locationFilter  = decodeURIComponent($stateParams.locationFilter) || "";
 
     searchCtrl.search = function() {
-      SearchService.search(searchCtrl.query)
+      SearchService.search(
+          searchCtrl.query,
+          searchCtrl.locationFilter,
+          searchCtrl.sortOption
+      )
       .then(function(videos) {
-        console.log('then!');
-        console.log(videos);
         searchCtrl.videos = videos;
       });
     };
 
     searchCtrl.doSearch = function() {
-      $state.go('search', {searchQuery: searchCtrl.query});
+      $state.go(
+        'search',
+        {
+          searchQuery: searchCtrl.query,
+          sortOption: searchCtrl.sortOption,
+          locationFilter: encodeURIComponent(searchCtrl.locationFilter)
+        },
+        {reload: true}
+      );
     };
 
     searchCtrl.search();
